bf32da5bde8790e181b5a44f185750c8deb01616
Fishook115 <martintellezapo@icloud.com>
2022-11-30 15:50:50 -0500

    Made Idle animation

---
+++ POE 2D/Assets/GrizzlynPlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrizzlynPlayerController : MonoBehaviour
+{
+    float movespeed = 1;
+
+    Animator
+
+    // initialization
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
