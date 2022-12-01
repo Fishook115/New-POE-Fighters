f4956eb012b1264333e6b571b506e69f65fdfbd9
CharlotteLewter <shashalewter@gmail.com>
2022-12-01 15:38:22 -0500

    Fixed the character hiding behind

---
+++ POE 2D/Assets/Scripts/CharacterSelectionScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelectionScript : MonoBehaviour
+{
+    public GameObject[] playerObjects;
+    public int selectedCharacter - 0;
+
+    public string gameScene "Character Selection Screen"
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
