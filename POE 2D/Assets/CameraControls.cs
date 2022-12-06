26ce84714f3a55da8516606049bc829c2609879a
Fishook115 <martintellezapo@icloud.com>
2022-12-06 15:02:50 -0500

    1

--- POE 2D/Assets/CameraControls.cs
+++ POE 2D/Assets/CameraControls.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
+
+    //Variables
+    public float cameraPanSpeed = -5f;
+    Vector2 startPos;
+    public Transform ground;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Variables
-        public float cameraPanSpeed = 1f;
+        //Start Position
+        startPos = transform.position;
 
-        //Camera Slides Down Towards the Ground
-
-
+    }
 
 
-}
-
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
-        
+        //Vector2 newPos = new Vector2(100, Time.deltaTime * cameraPanSpeed);
+        //transform.position = new Vector2(15, Time.deltaTime * cameraPanSpeed);
+        //startPos + Vector2.down * newPos;
+        transform.position = Vector3.Lerp(transform.position, ground.position, Time.deltaTime);
     }
 }
+
