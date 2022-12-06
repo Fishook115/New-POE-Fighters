7a0409968b182767b247a81b9350d5f53171eae9
Fishook115 <martintellezapo@icloud.com>
2022-12-06 15:48:22 -0500

    Working on player script

--- POE 2D/Assets/Scripts/GrizzlynPlayerController.cs
+++ POE 2D/Assets/Scripts/GrizzlynPlayerController.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class GrizzlynPlayerController : MonoBehaviour
 {
     float dirX, moveSpeed;
+
     Animator anim;
+
+    private Rigidbody2D rb;
 
     // Initialization
     void Start()
     {
         anim = GetComponent<Animator>();
         moveSpeed = 5f;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //player can move and punch
         dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
 
         transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
+
 
         if (dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
         {
@@ -35,6 +41,11 @@ public class GrizzlynPlayerController : MonoBehaviour
             anim.SetTrigger("Punch");
         }
 
+        //Check for collisions
+        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        
         
     }
+
 }
