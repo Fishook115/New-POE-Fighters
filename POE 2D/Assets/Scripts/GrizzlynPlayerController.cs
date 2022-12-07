bfb9514a49bd888df73c2a3d346af28c2a980d8a
Fishook115 <martintellezapo@icloud.com>
2022-12-07 15:52:35 -0500

    Player One Controller Script

--- POE 2D/Assets/Scripts/GrizzlynPlayerController.cs
+++ POE 2D/Assets/Scripts/GrizzlynPlayerController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class GrizzlynPlayerController : MonoBehaviour
 {
+    //move character
     float dirX, moveSpeed;
 
     Animator anim;
 
+<<<<<<< Updated upstream
+=======
+    //player one and player two rigid body
+>>>>>>> Stashed changes
     private Rigidbody2D rb;
 
     // Initialization
@@ -15,17 +20,31 @@ public class GrizzlynPlayerController : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         moveSpeed = 5f;
+<<<<<<< Updated upstream
         rb = GetComponent<Rigidbody2D>();
+=======
+
+        //player one and player 2 rigid body
+        rb = GetComponent<Rigidbody2D>();
+
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
         //player can move and punch
         dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+=======
+        if (name == "Fighter_Grizzlyn")
+        {
+            dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+>>>>>>> Stashed changes
 
-        transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
+            transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
 
+<<<<<<< Updated upstream
 
         if (dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
         {
@@ -46,6 +65,23 @@ public class GrizzlynPlayerController : MonoBehaviour
 
         
         
+=======
+            if (dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
+            {
+                anim.SetBool("isWalking", true);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+            }
+            if (Input.GetButtonDown("Fire1") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetTrigger("Punch");
+            }
+        }
+
+>>>>>>> Stashed changes
     }
 
 }
